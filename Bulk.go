4d654a2bc3216828f847ebd8c1e6cefa048f19ff
Chris Hurst <46472228+ytsruh@users.noreply.github.com>
2024-08-09 21:33:33 +0100

    feat: added new UI & bulk download functionality

---
+++ Bulk.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+	"ytsruh.com/backmeup/utils"
+)
+
+type BulkForm struct {
+	Urls string `form:"urls" validate:"required,url"`
+}
+
+func Bulk(c echo.Context) error {
+	var form BulkForm
+	err := c.Bind(&form)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+	// Check if form urls is valid and is not empty
+	if form.Urls == "" {
+		return c.String(http.StatusBadRequest, "urls are empty")
+	}
+	// Split urls by comma into a slice and trim whitespace
+	rawURLs := strings.Split(strings.TrimSpace(form.Urls), ",")
+	urls := make([]string, len(rawURLs))
+	for i, rawURL := range rawURLs {
+		urls[i] = strings.TrimSpace(rawURL)
+		// Check if url is valid and is not empty
+		if urls[i] == "" {
+			return c.String(http.StatusBadRequest, "urls are empty")
+		}
+		// Check if url is valid
+		if !strings.HasPrefix(urls[i], "http") {
+			return c.String(http.StatusBadRequest, "url is not valid")
+		}
+	}
+	//fmt.Printf("Urls = %v\n", len(urls))
+	//fmt.Println(urls)
+
+	// Check if url is a file, get the extension and save the file to temp directory
+	tempPath := "temp-" + utils.GenRandomString(10) + "/" // Create a new temp directory outside of range
+	for _, link := range urls {
+		u, err := url.Parse(link)
+		if err != nil {
+			return c.String(http.StatusBadRequest, "url could not be parsed")
+		}
+		extension := filepath.Ext(u.String())
+		if extension != ".pdf" && extension != ".xlsx" {
+			return c.String(http.StatusBadRequest, "url is not valid file. Only .pdf and .xlsx files are supported")
+		}
+		// Get file name & extension from url
+		filename := path.Base(u.Path)
+		//fmt.Println(filename)
+		if filename == "." || filename == "/" {
+			return c.String(http.StatusBadRequest, "url is not valid file, name could not be found")
+		} else {
+			extension := path.Ext(filename)
+			fileWithoutExt := filename[:len(filename)-len(extension)]
+			fmt.Println(fileWithoutExt)
+		}
+		// Create a new temp directory & get the file
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client := &http.Client{Transport: tr}
+		resp, err := client.Get(link)
+		if err != nil {
+			fmt.Printf("error downloading file: %s", err)
+			return c.String(http.StatusInternalServerError, "error downloading file")
+		}
+		defer resp.Body.Close()
+		err = os.MkdirAll(tempPath, 0755) // 0755 is the file permission (read and write permission)
+		if err != nil {
+			fmt.Printf("error creating temp directory: %s", err)
+			return c.String(http.StatusInternalServerError, "error creating a temp directory")
+		}
+		// Create a new file & copy the response body to the file
+		file, err := os.Create(tempPath + filename)
+		if err != nil {
+			fmt.Printf("error creating file: %s", err)
+			return c.String(http.StatusInternalServerError, "error creating a file")
+		}
+		defer file.Close()
+		// Copy the response body to the file
+		_, err = io.Copy(file, resp.Body)
+		if err != nil {
+			fmt.Printf("error copying file: %s", err)
+			return c.String(http.StatusInternalServerError, "error copying file")
+		}
+	}
+
+	// Create a new zip file
+	var dstfile = "zips/" + utils.GenRandomString(10) + ".zip"
+	err = utils.Zip(tempPath, dstfile)
+	if err != nil {
+		fmt.Printf("error creating zip file: %s", err)
+		return c.String(http.StatusInternalServerError, "error creating zip file")
+	}
+	// Remove all files from temp directory
+	err = os.RemoveAll(tempPath)
+	if err != nil {
+		fmt.Printf("error removing temp directory: %s", err)
+		return c.String(http.StatusInternalServerError, "error removing temp directory")
+	}
+
+	return c.String(http.StatusOK, dstfile)
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/echo/v4 v4.12.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -22,4 +23,5 @@ require (
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.23.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
